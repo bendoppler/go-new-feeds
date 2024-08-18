30bb1116e1e16fd2626085b288e10e65987b8598
Bao Do <thaibao.do@shopee.com>
2024-08-18 17:14:18 +0700

    Remove unused model

--- internal/api/model/friendModel.go
+++ internal/api/model/friendModel.go
@@ -1,16 +1,6 @@
 package model
 
-type FollowRequest struct {
-	FollowerID string `json:"followerId"`
-	FolloweeID string `json:"followeeId"`
-}
-
-type FriendListResponse struct {
-	UserID    string   `json:"userId"`
-	FriendIDs []string `json:"friendIds"`
-}
-
-type UserPostsResponse struct {
-	UserID string   `json:"userId"`
-	Posts  []string `json:"posts"` // This can be more detailed based on your post structure.
-}
+//type FollowRequest struct {
+//	FollowerID string `json:"followerId"`
+//	FolloweeID string `json:"followeeId"`
+//}
