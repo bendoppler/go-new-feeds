0349dc24928af8afc0a78c4c673505a1891078b1
Bao Do <thaibao.do@shopee.com>
2024-08-18 13:20:08 +0700

    Update user service

--- internal/db/mysqlDB.go
+++ internal/db/mysqlDB.go
@@ -31,37 +31,10 @@ func (factory *PersistentFactory) CreateMySQLDatabase() (*sql.DB, error) {
 	}
 
 	// Create table and index if not exists
-	err = createTableAndIndex(db)
+	err = Migrate(db)
 	if err != nil {
 		return nil, err
 	}
 
 	return db, nil
-}
-
-func createTableAndIndex(db *sql.DB) error {
-	// Create users table if not exists
-	createTableQuery := `CREATE TABLE IF NOT EXISTS users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(255) UNIQUE NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        first_name VARCHAR(255),
-        last_name VARCHAR(255),
-        birthday DATE,
-        password VARCHAR(255) NOT NULL
-    );`
-	_, err := db.Exec(createTableQuery)
-	if err != nil {
-		return fmt.Errorf("error creating table: %v", err)
-	}
-
-	// Optionally, create indices or other necessary structures
-	// Add full-text index on username if needed
-	createIndexQuery := `ALTER TABLE users ADD FULLTEXT(username);`
-	_, err = db.Exec(createIndexQuery)
-	if err != nil {
-		return fmt.Errorf("error creating full-text index: %v", err)
-	}
-
-	return nil
 }
